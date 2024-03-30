c6e0a47766ca338f942c4236ca248fcaeb0cc1c2
aliasbsv <aliasbsv@gmail.com>
2024-03-30 22:51:27 +0100

    d

--- js/task-3.js
+++ js/task-3.js
@@ -5,7 +5,6 @@ function getElementWidth(content, padding, border) {
   const totalWeight = cont + (pad * 2) + (bord * 2);
   return  totalWeight;
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+      console.log(getElementWidth("50px", "8px", "4px")); // 74
+      console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+      console.log(getElementWidth("200px", "0px", "0px")); // 200
